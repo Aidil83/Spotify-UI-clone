9eeb01357e5b6807edb5fc1bfdb94840ba0d69ad
Aidil <aidilrozi83@gmail.com>
2020-08-05 23:05:01 -0500

    Users can now access to their personal playlists

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,8 +4,11 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import { useDataLayerValue } from './DataLayer';
 
 function Sidebar() {
+	const [{ playlists }, dispatch] = useDataLayerValue();
+
 	return (
 		<div className='sidebar'>
 			<img
@@ -21,9 +24,9 @@ function Sidebar() {
 			<strong className='sidebar__title'>PLAYLISTS</strong>
 			<hr />
 
-			<SidebarOption title='Hip hop' />
-			<SidebarOption title='Rock' />
-			<SidebarOption title='RnB' />
+			{playlists?.items?.map((playlist) => (
+				<SidebarOption title={playlist.name} />
+			))}
 		</div>
 	);
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,8 @@ export const initialState = {
 	playing: false,
 	item: null,
 	// REMOVE after finished developing...
-	token:
-		'BQBA-i4Yme4qQ1EUfy8vwPIAuz0A_hbH9V378OnvEK0tjOopAsAunWmgukTtGsiXo64QBgdfLJSuqNajKSm90I7zSPRgbUvkFRX0f8SvK_urEfikJdHXsMP439_XJJfDmGsg1mWKBxehkzSYvC9UhP8_09uzvLBIzM74IeeqfMcd1OpGwKGV',
+	// token:
+	// 	'BQBA-i4Yme4qQ1EUfy8vwPIAuz0A_hbH9V378OnvEK0tjOopAsAunWmgukTtGsiXo64QBgdfLJSuqNajKSm90I7zSPRgbUvkFRX0f8SvK_urEfikJdHXsMP439_XJJfDmGsg1mWKBxehkzSYvC9UhP8_09uzvLBIzM74IeeqfMcd1OpGwKGV',
 };
 
 const reducer = (state, action) => {
@@ -21,6 +21,11 @@ const reducer = (state, action) => {
 			return {
 				...state,
 				token: action.token,
+			};
+		case 'SET_PLAYLISTS':
+			return {
+				...state,
+				playlists: action.playlists,
 			};
 		default:
 			return state;
