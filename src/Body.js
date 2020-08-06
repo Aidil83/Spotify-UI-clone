1fdd8ef1dff9655bce1d6b3415fee27d51dd68e0
Aidil <aidilrozi83@gmail.com>
2020-08-06 17:56:38 -0500

    Styled the Discover weekly banner

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,15 @@ function Body({ spotify }) {
 	return (
 		<div className='body'>
 			<Header spotify={spotify} />
+
+			<div className='body__info'>
+				<img src='https://i.scdn.co/image/6682aad217c11156e6d8985036996f1ea7ebb518' alt='' />
+				<div className='body__infoText'>
+					<strong>PLAYLIST</strong>
+					<h2>Discover Weekly</h2>
+					<p>description...</p>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,8 @@ export const initialState = {
 	playing: false,
 	item: null,
 	// REMOVE after finished developing...
-	// token:
-	// 	'BQBA-i4Yme4qQ1EUfy8vwPIAuz0A_hbH9V378OnvEK0tjOopAsAunWmgukTtGsiXo64QBgdfLJSuqNajKSm90I7zSPRgbUvkFRX0f8SvK_urEfikJdHXsMP439_XJJfDmGsg1mWKBxehkzSYvC9UhP8_09uzvLBIzM74IeeqfMcd1OpGwKGV',
+	token:
+		'BQBA-i4Yme4qQ1EUfy8vwPIAuz0A_hbH9V378OnvEK0tjOopAsAunWmgukTtGsiXo64QBgdfLJSuqNajKSm90I7zSPRgbUvkFRX0f8SvK_urEfikJdHXsMP439_XJJfDmGsg1mWKBxehkzSYvC9UhP8_09uzvLBIzM74IeeqfMcd1OpGwKGV',
 };
 
 const reducer = (state, action) => {
