54206b7a69b40a3dbadf6a46dd0e543f66a4a1d0
Aidil <aidilrozi83@gmail.com>
2020-09-06 04:52:33 -0500

    fixed the search bar

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from '@material-ui/icons/Search';
-import { Avatar } from '@material-ui/core';
-import { useDataLayerValue } from './DataLayer';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
 
 function Header() {
 	const [{ user }, dispatch] = useDataLayerValue();
 	return (
-		<div className='header'>
-			<div className='header__left'>
+		<div className="header">
+			<div className="header__left">
 				<SearchIcon />
-				<input placeholder='Search for Artist, Songs, or podcasts' type='text' />
+				<input placeholder="Search for Artist, Songs, or podcasts" type="text" />
 			</div>
 
-			<div className='header__right'>
+			<div className="header__right">
 				<Avatar src={user?.images[0]?.url} alt={user?.name} />
 				<h4>{user?.display_name}</h4>
 			</div>
